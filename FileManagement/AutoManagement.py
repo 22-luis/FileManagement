cd0bdbb9648928b4afc1f7d21b9c65ae19143e74
22-luis <00016720@uca.edu.sv>
2023-07-31 16:24:41 -0600

    first commit

---
+++ FileManagement/AutoManagement.py
@@ -0,0 +1,145 @@
+from os import scandir, rename
+from os.path import splitext, exists, join
+from shutil import move
+from time import sleep
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# We define here the destination of each download
+
+source_dir = r""
+dir_sfx = r""
+dir_music = r""
+dir_video = r""
+dir_images = r""
+dir_documents = r""
+dir_packages = r""
+dir_presentations = r""
+dir_excel = r""
+dir_pdf = r""
+dir_docs = r""
+
+# Here put all the extensions of the downloads
+
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd",
+                    ".raw", ".arw", ".cr2", ".nrw",
+                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf",
+                    ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+
+audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+
+document_extensions = [".odt", ".txt"]
+
+package_extensions = [".tar", ".zip", ".gz"]
+
+presentation_extensions = [".ppt", ".pptx"]
+
+excel_extensions = [".xls", ".xlsx"]
+
+pdf_extensions = [".pdf"]
+
+docs_extensions = [".doc", ".docx"]
+
+
+# This function verify is the document exist
+
+def make_unique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+
+    while exists(f"{dest}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+
+    return name
+
+
+# This function moves the repeat downloads
+
+def move_file(dest, name, entry):
+    if exists(f"{dest}/{name}"):
+        unique_name = make_unique(dest, name)
+        old_name = join(dest, name)
+        new_name = join(dest, unique_name)
+        rename(old_name, new_name)
+    move(entry, dest)
+
+
+# Create the class that moves the downloads
+
+class MoveHandler(FileSystemEventHandler):
+
+    def on_modified(self, event):
+        with scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                self.check_audio_files(entry, name)
+                self.check_video_files(entry, name)
+                self.check_image_files(entry, name)
+                self.check_document_files(entry, name)
+
+    def check_audio_files(self, entry, name):
+        global audio_extensions
+        for audio_extensions in audio_extensions:
+            if name.endswith(audio_extensions) or name.endswith(audio_extensions.upper()):
+                if entry.stat().st_size < 10_000_000 or "SFX" in name:
+                    dest = dir_sfx
+                else:
+                    dest = dir_music
+                move_file(dest, entry, name)
+                logging.info(f"Moved audio file: {name}")
+
+    def check_video_files(self, entry, name):
+        for video_extension in video_extensions:
+            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                move_file(dir_video, entry, name)
+                logging.info(f"Moved video file: {name}")
+
+    def check_image_files(self, entry, name):
+        for image_extension in image_extensions:
+            if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+                move_file(dir_images, entry, name)
+                logging.info(f"Moved image file: {name}")
+
+    def check_document_files(self, entry, name):
+        for documents_extension in document_extensions:
+            if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+                move_file(dir_documents, entry, name)
+                logging.info(f"Moved document file: {name}")
+            # Presentations
+            elif name.endswith(presentation_extensions) or name.endswith(presentation_extensions.upper()):
+                move_file(dir_presentations, entry, name)
+                logging.info(f"Moved document file: {name}")
+            elif name.endswith(excel_extensions) or name.endswith(excel_extensions.upper()):  # excels
+                move_file(dir_excel, entry, name)
+                logging.info(f"Moved document file: {name}")
+            elif name.endswith(pdf_extensions) or name.endswith(pdf_extensions.upper()):  # pdf
+                move_file(dir_pdf, entry, name)
+                logging.info(f"Moved document file: {name}")
+            elif name.endswith(docs_extensions) or name.endswith(docs_extensions.upper()):  # docs
+                move_file(dir_docs, entry, name)
+                logging.info(f"Moved document file: {name}")
+            elif name.endswith(package_extensions) or name.endswith(package_extensions.upper()):
+                move_file(dir_packages, entry, name)
+                logging.info(f"Moved document file: {name}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoveHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
